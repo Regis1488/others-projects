5c7b4e8c1813ec4ae5f63bc2276b06e1823eafa5
Regis1488 <82665748+Regis1488@users.noreply.github.com>
2021-12-13 19:12:43 +0100

    Update Calculator.cpp

--- cpp/Calculator.cpp
+++ cpp/Calculator.cpp
@@ -2,187 +2,147 @@
 #include <cmath>
 #include <thread>
 #include <chrono>
-
 using namespace std;
+
+
+
 void CheckOs() {
 #ifdef _WIN64
-	system("clear");
-	system("ConsoleApplication1.exe");
-#ifdef _WIN32
-	system("clear");
-	system("ConsoleApplication1.exe");
-#else
 	system("cls");
-	system("./calculator");
+#else
+	system("clear");
 #endif 
 
-	#endif
 }
 
 int main()
 {
-
-	CheckOs();
-	int reponse;
-	int i;
-	int resultat;
-	int xy;
-	int chiffre = 1;
-	float x;
-	double reponseTri;
-
-	cout << "###############################################\n#             Made by Sorus                   #\n#                                             #\n#                                             #\n#\##############################################\n";
-	cout << "Quelle operation veux tu effectuer" << endl;
-
-	cout << "[1] = Addition \n[2] = Soustraction\n[3] = Multiplication \n[4] = Division \n[5] = Racine carre\n[6] = Sinus\n[7] = Cosinus\n[8] = Tangente\n[9] = Credits\n[10] = Exit" << endl;
-	cin >> reponse;
-	if (reponse > 10) {
-		cout << "Vous n'avez pas choisi un chiffre correct" << endl;
-		return 0x02;
-	}
-
-
-	if (reponse == 1) {
-		cin.clear();
-		cout << "Combien de chiffre veux tu calculer ? (Default = 1)" << endl;
-		cin >> chiffre;
-		for (i = 0; i < chiffre; i++) {
-			cout << "Donne moi le premier chiffre a calculer" << endl;
-			while (!std::cin.good())
-			{
-				std::cin.clear();
-				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				std::cout << "Vous devez mettre un nombre" << std::endl;
-			}
-			cin >> x;
-			cout << "Donne moi le deuxieme chiffre a calculer" << endl;
-			cin >> xy;
-			resultat = x + xy;
-			cout << "Le resultat est " << resultat << endl;
-		}
-		this_thread::sleep_for(chrono::seconds(5));
+	int gotop = 0;
+beginning:
+	while (true) {
 		CheckOs();
-	}
-	if (reponse == 2) {
-		cin.clear();
-		cout << "Combien de chiffre veux tu calculer ? (Default = 1)" << endl;
-		cin >> chiffre;
-		for (i = 0; i < chiffre; i++) {
-			cout << "Donne moi le chiffre a calculer" << endl;
-			while (!std::cin.good())
-			{
-				std::cin.clear();
-				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				std::cout << "Vous devez mettre un nombre" << std::endl;
+		int reponse;
+		int i;
+		int resultat;
+		int xy;
+		int chiffre = 1;
+		float x;
+		double reponseTri;
 
-			}
+		cout << "###############################################\n#             Made by Sorus                   #\n#                                             #\n#                                             #\n#\##############################################\n";
+		cout << "Quelle operation veux tu effectuer" << endl;
 
-			cin >> x;
-			cout << "Donne moi le deuxieme chiffre a calculer" << endl;
-			cin >> xy;
-			resultat = x - xy;
-			cout << "Le resultat est " << resultat << endl;
+		cout << "[1] = Addition \n[2] = Soustraction\n[3] = Multiplication \n[4] = Division \n[5] = Racine carre\n[6] = Sinus\n[7] = Cosinus\n[8] = Tangente\n[9] = Credits\n[10] = Exit" << endl;
+		cin >> reponse;
+		if (reponse > 10) {
+			cout << "Vous n'avez pas choisi un chiffre correct" << endl;
+			return 0x02;
 		}
 
-		this_thread::sleep_for(chrono::seconds(5));
-		CheckOs();
-	}
-	if (reponse == 3) {
-		cin.clear();
-		cout << "Combien de chiffre veux tu calculer ? (Default = 1)" << endl;
-		cin >> chiffre;
-		for (i = 0; i < chiffre; i++) {
-			cout << "Donne moi le chiffre a calculer" << endl;
-			while (!std::cin.good())
-			{
-				std::cin.clear();
-				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				std::cout << "Vous devez mettre un nombre" << std::endl;
-			}
 
-			cin >> x;
-			cout << "Donne moi le deuxieme chiffre a calculer" << endl;
-			cin >> xy;
-			resultat = x * xy;
-			cout << "Le resultat est " << resultat << endl;
+		if (reponse == 1) {
+			cin.clear();
+			cout << "Combien de chiffre veux tu calculer ? (Default = 1)" << endl;
+			cin >> chiffre;
+			for (i = 0; i < chiffre; i++) {
+				cout << "Donne moi le premier chiffre a calculer" << endl;
+				while (!std::cin.good())
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::cout << "Vous devez mettre un nombre" << std::endl;
+				}
+				cin >> x;
+				cout << "Donne moi le deuxieme chiffre a calculer" << endl;
+				cin >> xy;
+				resultat = x + xy;
+				cout << "Le resultat est " << resultat << endl;
+			}
+			this_thread::sleep_for(chrono::seconds(5));
+			CheckOs();
+			goto beginning;
+
+			return 0;
 
 		}
-		this_thread::sleep_for(chrono::seconds(5));
-		CheckOs();
-	}
-	if (reponse == 4) {
-		cin.clear();
-		cout << "Combien de chiffre veux tu calculer ? (Default = 1)" << endl;
-		cin >> chiffre;
-		for (i = 0; i < chiffre; i++) {
-			cout << "Donne moi le chiffre a calculer" << endl;
-			while (!std::cin.good())
-			{
-				std::cin.clear();
-				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				std::cout << "Vous devez mettre un nombre" << endl;
+		if (reponse == 2) {
+			cin.clear();
+			cout << "Combien de chiffre veux tu calculer ? (Default = 1)" << endl;
+			cin >> chiffre;
+			for (i = 0; i < chiffre; i++) {
+				cout << "Donne moi le chiffre a calculer" << endl;
+				while (!std::cin.good())
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::cout << "Vous devez mettre un nombre" << std::endl;
 
-			}
-			cin >> x;
-			cout << "Donne moi le deuxieme chiffre a calculer" << endl;
-			cin >> xy;
-			resultat = x / xy;
-			cout << "Le resultat est " << resultat << endl;
+				}
 
-		}
-		this_thread::sleep_for(chrono::seconds(5));
-		CheckOs();
-	}
-	if (reponse == 5) {
-		cin.clear();
-		cout << "Combien de chiffre veux tu calculer ?" << endl;
-		cin >> chiffre;
-		cout << "Donne moi le chiffre a calculer " << endl;
-		while (!std::cin.good())
-		{
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			std::cout << "Vous devez mettre un nombre" << std::endl;
-			std::cin >> chiffre;
-		}
-		for (i = 0; i < chiffre; i++) {
-			while (!std::cin.good())
-			{
-				std::cin.clear();
-				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				std::cout << "Vous devez mettre un nombre" << std::endl;
-				std::cin >> chiffre;
+				cin >> x;
+				cout << "Donne moi le deuxieme chiffre a calculer" << endl;
+				cin >> xy;
+				resultat = x - xy;
+				cout << "Le resultat est " << resultat << endl;
 			}
 
-			cin >> x;
-			xy = x;
-			if (x == 0) {
-				cout << "Il n'y a pas de racine carre" << endl;
-				cout << "Donne moi le chiffres a calculer " << endl;
-			}
-			else {
-				x = sqrt(x);
-				cout << "Le resultat de la racine carre de " << xy << " est : " << x << endl;
+			this_thread::sleep_for(chrono::seconds(5));
+			CheckOs();
+
+		}
+		if (reponse == 3) {
+			cin.clear();
+			cout << "Combien de chiffre veux tu calculer ? (Default = 1)" << endl;
+			cin >> chiffre;
+			for (i = 0; i < chiffre; i++) {
+				cout << "Donne moi le chiffre a calculer" << endl;
+				while (!std::cin.good())
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::cout << "Vous devez mettre un nombre" << std::endl;
+				}
+
+				cin >> x;
+				cout << "Donne moi le deuxieme chiffre a calculer" << endl;
+				cin >> xy;
+				resultat = x * xy;
+				cout << "Le resultat est " << resultat << endl;
 
 			}
+			this_thread::sleep_for(chrono::seconds(5));
+			CheckOs();
+
 		}
-		this_thread::sleep_for(chrono::seconds(5));
-		CheckOs();
-	}
-	if (reponse == 6)
-	{
-		cin.clear();
-		cout << "Vous voulez calculer combien de chiffre ?" << endl;
-		cin >> chiffre;
-		while (!std::cin.good())
-		{
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			cout << "Vous devez mettre un nombre" << endl;
-			std::cin >> chiffre;
+		if (reponse == 4) {
+			cin.clear();
+			cout << "Combien de chiffre veux tu calculer ? (Default = 1)" << endl;
+			cin >> chiffre;
+			for (i = 0; i < chiffre; i++) {
+				cout << "Donne moi le chiffre a calculer" << endl;
+				while (!std::cin.good())
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::cout << "Vous devez mettre un nombre" << endl;
+
+				}
+				cin >> x;
+				cout << "Donne moi le deuxieme chiffre a calculer" << endl;
+				cin >> xy;
+				resultat = x / xy;
+				cout << "Le resultat est " << resultat << endl;
+
+			}
+			this_thread::sleep_for(chrono::seconds(5));
+			CheckOs();
+
 		}
-		cout << "Veuiller saisir le chiffre a calculer" << endl;
-		for (i = 0; i < chiffre; i++) {
+		if (reponse == 5) {
+			cin.clear();
+			cout << "Combien de chiffre veux tu calculer ?" << endl;
+			cin >> chiffre;
+			cout << "Donne moi le chiffre a calculer " << endl;
 			while (!std::cin.good())
 			{
 				std::cin.clear();
@@ -190,88 +150,136 @@ int main()
 				std::cout << "Vous devez mettre un nombre" << std::endl;
 				std::cin >> chiffre;
 			}
+			for (i = 0; i < chiffre; i++) {
+				while (!std::cin.good())
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::cout << "Vous devez mettre un nombre" << std::endl;
+					std::cin >> chiffre;
+				}
 
+				cin >> x;
+				xy = x;
+				if (x == 0) {
+					cout << "Il n'y a pas de racine carre" << endl;
+					cout << "Donne moi le chiffres a calculer " << endl;
+				}
+				else {
+					x = sqrt(x);
+					cout << "Le resultat de la racine carre de " << xy << " est : " << x << endl;
 
-
-			cin >> reponse;
-			reponseTri = sin(reponse);
-			cout << reponseTri << endl;
+				}
+			}
+			this_thread::sleep_for(chrono::seconds(5));
+			CheckOs();
 		}
-		this_thread::sleep_for(chrono::seconds(5));
-		CheckOs();
-	}
-	if (reponse == 7)
-	{
-		cin.clear();
-		cout << "Vous voulez calculer combien de chiffre ?" << endl;
-		cin >> chiffre;
-		while (!std::cin.good())
+		if (reponse == 6)
 		{
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			cout << "Vous devez mettre un nombre" << endl;
-			std::cin >> chiffre;
-		}
-		cout << "Veuiller saisir le chiffre a calculer" << endl;
-		for (i = 0; i < chiffre; i++) {
+			cin.clear();
+			cout << "Vous voulez calculer combien de chiffre ?" << endl;
+			cin >> chiffre;
 			while (!std::cin.good())
 			{
 				std::cin.clear();
 				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				std::cout << "Vous devez mettre un nombre" << std::endl;
+				cout << "Vous devez mettre un nombre" << endl;
 				std::cin >> chiffre;
 			}
+			cout << "Veuiller saisir le chiffre a calculer" << endl;
+			for (i = 0; i < chiffre; i++) {
+				while (!std::cin.good())
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::cout << "Vous devez mettre un nombre" << std::endl;
+					std::cin >> chiffre;
+				}
 
 
 
-			cin >> reponse;
-			reponseTri = cos(reponse);
-			cout << reponseTri << endl;
+				cin >> reponse;
+				reponseTri = sin(reponse);
+				cout << reponseTri << endl;
+			}
+			this_thread::sleep_for(chrono::seconds(5));
+			CheckOs();
+
 		}
-		this_thread::sleep_for(chrono::seconds(5));
-		CheckOs();
-	}
-	if (reponse == 8)
-	{
-		cin.clear();
-		cout << "Vous voulez calculer combien de chiffre ?" << endl;
-		cin >> chiffre;
-		while (!std::cin.good())
+		if (reponse == 7)
 		{
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			cout << "Vous devez mettre un nombre" << endl;
-			std::cin >> chiffre;
-		}
-		cout << "Veuiller saisir le chiffre a calculer" << endl;
-		for (i = 0; i < chiffre; i++) {
+			cin.clear();
+			cout << "Vous voulez calculer combien de chiffre ?" << endl;
+			cin >> chiffre;
 			while (!std::cin.good())
 			{
 				std::cin.clear();
 				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				std::cout << "Vous devez mettre un nombre" << std::endl;
+				cout << "Vous devez mettre un nombre" << endl;
 				std::cin >> chiffre;
 			}
+			cout << "Veuiller saisir le chiffre a calculer" << endl;
+			for (i = 0; i < chiffre; i++) {
+				while (!std::cin.good())
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::cout << "Vous devez mettre un nombre" << std::endl;
+					std::cin >> chiffre;
+				}
 
 
 
-			cin >> reponse;
-			reponseTri = tan(reponse);
-			cout << reponseTri << endl;
+				cin >> reponse;
+				reponseTri = cos(reponse);
+				cout << reponseTri << endl;
+			}
+			this_thread::sleep_for(chrono::seconds(5));
+			CheckOs();
+
 		}
-		this_thread::sleep_for(chrono::seconds(5));
-		CheckOs();
-	}
-	if (reponse == 9) {
-		cout << "Créateur et Devloppeur : \n Titouan Dupont\n Alias Sorus\n ou Regis1488" << endl;
-		this_thread::sleep_for(chrono::seconds(5));
-		CheckOs();
+		if (reponse == 8)
+		{
+			cin.clear();
+			cout << "Vous voulez calculer combien de chiffre ?" << endl;
+			cin >> chiffre;
+			while (!std::cin.good())
+			{
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				cout << "Vous devez mettre un nombre" << endl;
+				std::cin >> chiffre;
+			}
+			cout << "Veuiller saisir le chiffre a calculer" << endl;
+			for (i = 0; i < chiffre; i++) {
+				while (!std::cin.good())
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::cout << "Vous devez mettre un nombre" << std::endl;
+					std::cin >> chiffre;
+				}
 
-	}
-	if (reponse == 10) {
-		cout << "Arevoir !" << endl;
-		this_thread::sleep_for(chrono::seconds(1));
-		CheckOs();
-		return 0x01;
+
+
+				cin >> reponse;
+				reponseTri = tan(reponse);
+				cout << reponseTri << endl;
+			}
+			this_thread::sleep_for(chrono::seconds(5));
+			CheckOs();
+		}
+		if (reponse == 9) {
+			cout << "Créateur et Devloppeur : \n Titouan Dupont\n Alias Sorus\n ou Regis1488" << endl;
+			this_thread::sleep_for(chrono::seconds(5));
+			CheckOs();
+
+		}
+		if (reponse == 10) {
+			cout << "Arevoir !" << endl;
+			this_thread::sleep_for(chrono::seconds(1));
+			CheckOs();
+			return 0x01;
+		}
 	}
 }
