dab4d97112bc4320eef396475508ae32b90e949f
Regis1488 <82665748+Regis1488@users.noreply.github.com>
2021-12-14 22:04:59 +0100

    Add files via upload

---
+++ cpp/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace aya
+{
+ 
+    public partial class Form1 : Form
+    {
+        int y;
+        int x;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Entrer uniquement un nombre!");
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                return;
+            }
+            try
+            {
+                y = Int32.Parse(textBox1.Text);
+            }
+            catch
+            {
+
+            }
+            
+            Application.DoEvents();
+
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Entrer uniquement un nombre!");
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+            try
+            {
+                x = Int32.Parse(textBox2.Text);
+            } catch
+            {
+                
+            }
+            
+            Application.DoEvents();
+            if (checkBox1.Checked == true)
+            {
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+                checkBox5.Checked = false;
+                checkBox6.Checked = false;
+                checkBox7.Checked = false;
+                double resultat = x + y;
+            textBox3.Text = resultat.ToString();
+   
+            }
+            if(checkBox2.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+                checkBox5.Checked = false;
+                checkBox6.Checked = false;
+                checkBox7.Checked = false;
+                double resultat = y - x;
+                textBox3.Text = resultat.ToString();
+            }
+        if(checkBox3.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox4.Checked = false;
+                checkBox5.Checked = false;
+                checkBox6.Checked = false;
+                checkBox7.Checked = false;
+                double resultat = x * y;
+                textBox3.Text = resultat.ToString();
+            }
+        if(checkBox4.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox5.Checked = false;
+                checkBox6.Checked = false;
+                checkBox7.Checked = false;
+                double resultat = y/x;
+                textBox3.Text = resultat.ToString();
+            }
+        if(checkBox5.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+                checkBox6.Checked = false;
+                checkBox7.Checked = false;
+                double resultat = Math.Sqrt(y / x);
+                    textBox3.Text = resultat.ToString();
+                
+            }
+            if (checkBox6.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+                checkBox5.Checked = false;
+                checkBox6.Checked = false;
+                checkBox7.Checked = false;
+                double resultat = Math.Cos(y / x);
+                textBox3.Text = resultat.ToString();
+
+            }
+            if (checkBox7.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+                checkBox5.Checked = false;
+                checkBox6.Checked = false;
+                double resultat = Math.Sin(y / x);
+                textBox3.Text = resultat.ToString();
+
+            }
+            if (checkBox1.Checked == false && checkBox2.Checked == false && checkBox3.Checked == false && checkBox4.Checked == false && checkBox5.Checked == false && checkBox6.Checked == false && checkBox7.Checked == false)
+
+            {
+                textBox3.Clear();
+            }
+        }
+        
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+            checkBox6.Checked = false;
+            checkBox7.Checked = false;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox6.Checked = false;
+            checkBox7.Checked = false;
+            checkBox5.Checked = false;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+            checkBox6.Checked = false;
+            checkBox7.Checked = false;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox5.Checked = false;
+            checkBox6.Checked = false;
+            checkBox7.Checked = false;
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox6.Checked = false;
+            checkBox7.Checked = false;
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+            checkBox7.Checked = false;
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+            checkBox6.Checked = false;
+        }
+
+    }
+}
